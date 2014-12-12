29a1c666ddced8f869a1c4019e63a23a59100fe9
Ben Gitles <bengitles@gmail.com>
2014-12-11 21:21:33 -0500

    Created text file with all courses

--- get_dept_courses.py
+++ get_dept_courses.py
@@ -2,8 +2,18 @@ import json
 
 def get_courses(filename) :
 	data = json.load(open(filename))
-	courses = {}
-	print data[result_data][0]
+	courses = []
+	for course in data["result_data"]:
+		courses.append(course["course_department"] + course["course_number"])
+	return courses
 
+departments = open('departments.txt')
+all_courses = []
+for dept in departments.readlines():
+	all_courses.append(get_courses("courseInfoJSON/" + dept.strip() + ".json"))
 
-get_courses("courseInfoJSON/MATH.json")
+file = open("all_courses.txt", "w")
+for dept in all_courses:
+	for course in dept:
+		file.write(course + '\n')
+file.close()
