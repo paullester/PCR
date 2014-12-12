3b8e731d788ead1c5bef3c69e4947b89f2ab6ee6
Ben Gitles <bengitles@gmail.com>
2014-12-11 23:21:00 -0500

    Updated all_courses; should be exhaustive now

--- get_groups.py
+++ get_groups.py
@@ -7,10 +7,15 @@ def print_courses(dept, exclusions, minimum) :
 			if num >= minimum and not num in exclusions:
 				print "\"" + dept + str(num) + "\","
 
+def print_all_courses(dept) :
+	for course in courses.readlines():
+		if course.startswith(dept) and course[len(dept):].strip().isdigit():
+			print "\"" + course.strip() + "\","
+
 #Engineering Natural Science Requirements:
 #print_courses("ENM",[],0)
 #print_courses("MATH",[115, 122, 123, 130, 150, 151, 170, 172, 174, 180, 210, 212, 220, 475],104)
-#print_courses("BCHE",[],0)
+#print_all_courses("BCHE")
 #print_courses("BIBB",[10, 50, 60, 227],0)
 #print_courses("BIOL",[130, 446, 544],101)
 #print_courses("BMB",[],0)
@@ -38,7 +43,60 @@ def print_courses(dept, exclusions, minimum) :
 #print_courses('ENGR',[296,297],0)
 #print_courses('CBE',[296,297],0)
 #print_courses('MSE',[296,297],0)
-print_courses('NETS',[296,297],0)
+#print_courses('NETS',[296,297],0)
 
 #Tech Elective
 #print_courses("MATH",[],0)
+
+#Engineering Social Sciences:
+#print_all_courses("ASAM")
+print_all_courses("COMM")
+print_all_courses("CRIM")
+print_all_courses("ECON")
+print_all_courses("GSWS")
+print_all_courses("HSOC")
+print_all_courses("INTR")
+print_all_courses("LING")
+print_all_courses("PPE")
+print_all_courses("PSCI")
+print_all_courses("PSYC")
+print_all_courses("SOCI")
+print_all_courses("STSC")
+print_all_courses("URBS")
+
+#Engineering Humanities
+print_all_courses("ANTH")
+print_all_courses("ANCH")
+print_all_courses("ANEL")
+print_all_courses("ARTH")
+print_all_courses("CLST")
+print_all_courses("COML")
+print_all_courses("EALC")
+print_all_courses("ENGL")
+print_all_courses("FNAR")
+print_all_courses("FOLK")
+print_all_courses("GRMN")
+print_all_courses("DTCH")
+print_all_courses("SCND")
+print_all_courses("HIST")
+print_all_courses("HSSC")
+print_all_courses("JWST")
+print_all_courses("LALS")
+print_all_courses("MUSC")
+print_all_courses("NELC")
+print_courses("PHIL",[5,6],0)
+print_all_courses("RELS")
+print_all_courses("FREN")
+print_all_courses("ITAL")
+print_all_courses("PRTG")
+print_all_courses("ROML")
+print_all_courses("SPAN")
+print_all_courses("CZECH")
+print_all_courses("EEUR")
+print_all_courses("POLISH")
+print_all_courses("RUSS")
+print_all_courses("SLAV")
+print_all_courses("SARS")
+print_all_courses("SAST")
+print_all_courses("THAR")
+print_all_courses("VLST")
