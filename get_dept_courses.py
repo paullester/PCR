fb444d7026af8edeb34e24468c77c274634ae90d
speedygeek <hongjos@seas.upenn.edu>
2014-12-24 21:37:27 -0500

    Revert "Merge branch 'master' of https://github.com/paullester/PCR"
    This reverts commit e536bbc0b27e549e4a046a174e4cbfa57059a9dc, reversing
    changes made to d2ac868fa6afc073c222c9140c7ae693a6fb451a.

--- get_dept_courses.py
+++ get_dept_courses.py
@@ -4,13 +4,13 @@ def get_courses(filename) :
 	data = json.load(open(filename))
 	courses = []
 	for course in data["result_data"]:
-		courses.append(course["course_id"])
+		courses.append(course["course_department"] + course["course_number"])
 	return courses
 
 departments = open('departments.txt')
 all_courses = []
 for dept in departments.readlines():
-	all_courses.append(get_courses("departmentJSON/" + dept.strip() + ".json"))
+	all_courses.append(get_courses("courseInfoJSON/" + dept.strip() + ".json"))
 
 all_unique_courses = []
 for dept in all_courses:
