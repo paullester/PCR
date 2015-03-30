307efe9eea4e18c0501cbda389649157232f9a91
paullester <Paul@hnt-upper240.apn.wireless-pennnet.upenn.edu>
2015-03-29 20:10:12 -0400

    updating courses and students objects

---
+++ recommender/senior_design/src/multiattribute/Student.java
@@ -0,0 +1,14 @@
+package multiattribute;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Student {
+	
+	private ArrayList<String> interests;
+	private Set<Course> courses_taken;
+	private Major major;
+	
+	
+
+}
