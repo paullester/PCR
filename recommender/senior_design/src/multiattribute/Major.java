7c31d52ffb80d8533a32da8f19199ecf7433532d
Ben Gitles <bengitles@gmail.com>
2015-03-29 20:09:39 -0400

    Adding skeleton of Majors.java

---
+++ recommender/senior_design/src/multiattribute/Major.java
@@ -0,0 +1,8 @@
+public class Major {
+
+	private String name;
+	private Set<Set<Course>> requirements;
+	private Map<Course, Integer> numDescendants;
+	private String url;
+	private Set<String> notes;
+}
