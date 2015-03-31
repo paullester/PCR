0c15bfcf9d22fce5c46d37a6dbe43fc305fea85f
speedygeek <hongjos@seas.upenn.edu>
2015-03-30 23:07:13 -0400

    .

--- src/PennCourseRecommender/InterestScore.java
+++ src/PennCourseRecommender/InterestScore.java
@@ -3,12 +3,12 @@ package src.PennCourseRecommender;
 import java.util.List;
 
 
-//TFIDF MODULE
+//src.TFIDF.TFIDF MODULE
 
 
 public interface InterestScore {
 	
-	//takes in a course and a student. Based on the student's interests and course desc, use TFIDF to return normalized score betwee 0 and 1. 
+	//takes in a course and a student. Based on the student's interests and course desc, use TFIDF to return normalized score betwee 0 and 1.
 	public double getInterestScore(Course c, Student s);
 
 }
