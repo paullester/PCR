8fa2c2ce451e5f4cb12d74ba85fa7678020b4888
paullester <Paul@hnt-upper468.apn.wireless-pennnet.upenn.edu>
2015-04-08 19:10:45 -0400

    integrating ben's code, paul's course req tree not complete yet

--- src/PennCourseRecommender/ReqTree.java
+++ src/PennCourseRecommender/ReqTree.java
@@ -1,8 +1,10 @@
 package PennCourseRecommender;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.json.simple.JSONArray;
@@ -22,12 +24,9 @@ public class ReqTree {
 		this.children = new HashSet<ReqTree>();
 		for (String key : (Set<String>) reqs.keySet()) {
 			if (!key.equals("URL") && !key.equals("Notes")) {
-				//this.children.add(new ReqTree(reqs.get(key), this, this.level + 1));
 				if (reqs.get(key) instanceof JSONArray) {
 					JSONArray x = (JSONArray) reqs.get(key);
-					//for (int i=1; i < x.size(); i++) {
-						this.children.add(new ReqTree(x, this, this.level + 1));
-					//}
+					this.children.add(new ReqTree(x, this, this.level + 1));
 				} else {
 					this.children.add(new ReqTree(reqs.get(key), this, this.level + 1));
 				}
@@ -42,18 +41,14 @@ public class ReqTree {
 		if (o instanceof String) {
 			this.children = null;
 			String s = (String) o;
-			this.data = s;
-			/*if (!s.startsWith("G: ")) {
+			if (!s.startsWith("G: ")) {
 				this.data = s;
 			} else {
-				JSONArray groupCourses =
-						(JSONArray) this.parent.getGroups().get(s.substring(3));
-				for (int i = 0; i < groupCourses.size(); i++) {
-					ReqTree grandparent = null;
-					if (this.parent.getParent() != null) grandparent = this.parent.getParent();
-					this.parent.addChild(new ReqTree(groupCourses.get(i), grandparent, this.level));
+				JSONArray groupCourses = this.getGroup(s.substring(3));
+				for (int i = 0; groupCourses != null && i < groupCourses.size(); i++) {
+					this.parent.addChild(new ReqTree(groupCourses.get(i), this.parent, this.level));
 				}
-			}*/
+			}
 		} else if (o instanceof JSONArray) {
 			JSONArray subReq = (JSONArray) o;
 			if (subReq.size() > 0) {
@@ -68,6 +63,8 @@ public class ReqTree {
 						this.children.add(new ReqTree(subReq.get(i), this, this.level + 1));
 					}
 				}
+			} else {
+				//Figure out what to do about Free Electives
 			}
 		} else {
 			System.out.println("What did you pass in?");
@@ -106,9 +103,9 @@ public class ReqTree {
 		return this.parent;
 	}
 	
-	public JSONObject getGroups() {
-		if (this.groups == null) return this.parent.getGroups();
-		return this.groups;
+	public JSONArray getGroup(String group) {
+		if (this.groups == null) return this.parent.getGroup(group);
+		return (JSONArray) this.groups.get(group);
 	}
 	
 	public void addChild(ReqTree child) {
@@ -126,5 +123,42 @@ public class ReqTree {
 			}
 		}
 		return s;
+	}
+	
+	public Map<String, Double> getDescendantScores() {
+		//this is totally wrong now. I need to look up prerequisites.
+		Map<String, Double> dScores = new HashMap<String, Double>();
+		if (this.children != null) {
+			for (ReqTree child : this.children) {
+				Map<String, Double> childDScores = child.getDescendantScores();
+				for (String course : childDScores.keySet()) {
+					if (this.data.equals("OR")) {
+						if (dScores.containsKey(course)) {
+							dScores.put(course, dScores.get(course) + childDScores.get(course) / childDScores.keySet().size());
+						} else {
+							dScores.put(course, 1.0 / childDScores.keySet().size());
+						}
+					} else if (this.data.equals("AND")) {
+						if (dScores.containsKey(course)) {
+							dScores.put(course, dScores.get(course) + childDScores.get(course));
+						} else {
+							dScores.put(course, childDScores.get(course));
+						}
+					} else if (this.data.startsWith("OF")) {
+						Double numOf = Double.valueOf(this.data.charAt(2));
+						if (dScores.containsKey(course)) {
+							dScores.put(course, dScores.get(course) + childDScores.get(course) * numOf / childDScores.keySet().size());
+						} else {
+							dScores.put(course, childDScores.get(course) * numOf / childDScores.keySet().size());
+						}
+					} else {
+						System.out.println("How did we get here?");
+					}
+				}
+			}
+		} else {
+			dScores.put(this.data, 1.0);
+		}
+		return dScores;
 	}
 }
