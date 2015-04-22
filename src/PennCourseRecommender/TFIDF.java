95e32a5e7b3ff20db0fe93bd032e372f188ccdd5
Susan Greenberg <susangr@seas.upenn.edu>
2015-04-22 18:49:47 -0400

    shit

--- src/PennCourseRecommender/TFIDF.java
+++ src/PennCourseRecommender/TFIDF.java
@@ -1,5 +1,7 @@
+package tmp;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-
+import java.io.IOException;
 import java.util.*;
 
 import org.json.simple.JSONArray;
@@ -16,14 +18,14 @@ public class TFIDF {
     // Instanitate with keyset of feasible courses
     public TFIDF(Set<String> interests, Set<String> feasibleCourses) {
         try {
-            reader = new FileReader("allCourses.json") //set path?
+            reader = new FileReader("allCourses.json"); 
             parser = new JSONParser;
             Object obj = parser.parse(reader);
             JSONObject jObj = (JSONObject) obj; 
             Iterator<JSONObject> keys = jObj.keys();
-            Map<String, String[]> courseDesc = new HashMap<String, String>();
+            Map<String, String[]> courseDesc = new HashMap<String, String[]>();
             genFeasDesc(keys, feasibleCourses, courseDesc);
-            Map<String, Map<String, int>> courseCount = new HashMap<String, Map<String, int>>();
+            Map<String, Map<String, Integer>> courseCount = new HashMap<String, Map<String, Integer>>();
             countCourses(interests, courseCount, courseDesc);
             getScoresAndOrder(courseCount);
         } catch (FileNotFoundException e) {
@@ -32,10 +34,7 @@ public class TFIDF {
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        } 
     }
 
     public Map<String, List<String>> getRankedOrder() {
@@ -46,7 +45,7 @@ public class TFIDF {
             Map<String, String[]> courseDesc) {
         while (keys.hasNext) {
             JSONObject course = keys.next();
-            if (feasiblecourse.contains(course.get("course_id").toString()) {
+            if (feasibleCourses.contains(course.get("course_id").toString())) {
                 courseDesc.put(course.get("course_id").toString(), 
                         course.get("course_description").toString().split(" "));
             }
@@ -54,9 +53,9 @@ public class TFIDF {
         }
     }
 
-    private void countCourses(Set<String> interests, Map<String, Map<String, int>> courseCount, 
+    private void countCourses(Set<String> interests, Map<String, Map<String, Integer>> courseCount, 
             Map<String, String[]> courseDesc) {
-        Map<String, int> maxMap = new HashMap<String, int>();
+        Map<String, Integer> maxMap = new HashMap<String, Integer>();
         maxMap.put("max", 0);
         for (String interest : interests) {
             courseCount.put(interest, maxMap);
@@ -66,28 +65,28 @@ public class TFIDF {
             for (String word : desc) {
                 if (interests.contains(word)) {
                     int count = 0;
-                    if (courseCount.contains(word)) {
-                        if (courseCount.get(word).contains(course)) {
-                            Map<String, int> tmp = courseCount.get(word);
+                    if (courseCount.containsKey(word)) {
+                        if (courseCount.get(word).containsKey(course)) {
+                            Map<String, Integer> tmp = courseCount.get(word);
                             count = tmp.get(course) + 1;
                             tmp.put(course, count);
                             courseCount.put(word, tmp);
                         } else {
-                            Map<String, int> tmp = new HashMap<String, int>();
+                            Map<String, Integer> tmp = new HashMap<String, Integer>();
                             count = 1;
                             tmp.put(course, count);
                             courseCount.put(word, tmp);
                         }
                         if (count > courseCount.get(word).get("max")) {
-                            Map<String, int> tmp = new Hashmap<String, int>();
+                            Map<String, Integer> tmp = new HashMap<String, Integer>();
                             tmp.put("max", count);
                             courseCount.put(word, tmp);
                         } 
                     } else {
-                        Map<String, int> tmp = new HashMap<String, int>();
+                        Map<String, Integer> tmp = new HashMap<String, Integer>();
                         tmp.put(course, 1);
                         courseCount.put(word, tmp);
-                        Map<String, int> tmp2 = new HashMap<String, int>();
+                        Map<String, Integer> tmp2 = new HashMap<String, Integer>();
                         tmp2.put("max", 1);
                         courseCount.put(word, tmp2);
                     }
@@ -96,28 +95,28 @@ public class TFIDF {
         }
     }
 
-    private void getScoresAndOrder(Map<String, Map<String, int>> courseCount) {
-        Map<double, List<String>> tree = new TreeMap<Double, List<String>>(); 
+    private void getScoresAndOrder(Map<String, Map<String, Integer>> courseCount) {
+        Map<Double, List<String>> tree = new TreeMap<Double, List<String>>(); 
         for (String interest : courseCount.keySet()) {
-            int max = courseCount.get(interest).get("max");
+            Integer max = courseCount.get(interest).get("max");
             courseCount.get(interest).remove("max");
             for (String course : courseCount.get(interest).keySet()) {
-                int count = courseCount.get(interest).get(course);
-                double weight = (0.5 + (0.5) * (double)(count / max)) 
-                    * Math.log((double) count / (courseCount.get(interest).keySet().length));
-                if (tree.contains(weight)) {
+                Integer count = courseCount.get(interest).get(course);
+                Double weight = (0.5 + (0.5) * (double)(count / max)) 
+                    * Math.log((double) count / (courseCount.get(interest).keySet().size()));
+                if (tree.containsKey(weight)) {
                     List<String> tmp = tree.get(weight);
                     tmp.add(course);
                     tree.put(weight, tmp);
                 } else {
-                    List<String> tmp = new List<String>();
+                    List<String> tmp = new LinkedList<String>();
                     tmp.add(course);
                     tree.put(weight, tmp);
                 }
             }
             List<String> orderedCourses = new LinkedList<String>();
-            for (double weight : tree.descendingKeySet()) {
-                orderedCourses.add(tree.get(weight));
+            for (Double weight : ((TreeMap<Double, List<String>>) tree).descendingKeySet()) {
+                orderedCourses.addAll(tree.get(weight));
             }
             ranked.put(interest, orderedCourses);
         }
